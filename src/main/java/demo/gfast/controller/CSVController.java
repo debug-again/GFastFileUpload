38cff49449f280406f25525da61b995f399948e4
Goutam Singh <debug.again@gmail.com>
2016-08-02 16:35:41 +0530

    Initial commit

---
+++ src/main/java/demo/gfast/controller/CSVController.java
@@ -0,0 +1,47 @@
+package demo.gfast.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/csv")
+public class CSVController {
+
+    private final String uploadDirectory = "C:\\Gfast\\";
+
+    private void log(String message) {
+        System.out.println(message);
+    }
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public ResponseEntity<Map<String, String>> uploadFileHandler(@RequestParam("name") String name, @RequestParam("file") MultipartFile file) {
+
+        log("Inside CSVController.uploadFileHandler");
+        log("name = [" + name + "]");
+        Map<String, String> res = new HashMap<String, String>();
+        File desFile = new File(uploadDirectory + file.getOriginalFilename());
+
+        try {
+            file.transferTo(desFile);
+            res.put("status", "success");
+        } catch (IOException e) {
+            log("Unable to transger object at " + desFile.getAbsolutePath());
+            log("Exception received : " + e.getMessage());
+            e.printStackTrace();
+            res.put("status", "failure");
+            return new ResponseEntity<Map<String, String>>(res, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<Map<String, String>>(res, HttpStatus.CREATED);
+    }
+}
---
+++ web/js/gfast.js
@@ -0,0 +1,49 @@
+(function(){
+
+    var app = angular.module('app', []);
+
+    /* Directive */
+    app.directive('fileModel', ['$parse', function($parse){
+        return{
+            restrict : 'A',
+            link : function (scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                element.bind('change', function(){
+                    scope.$apply(function(){
+                        model.assign(scope, element[0].files[0]);
+                    });
+                });
+            }
+        };
+    }]);
+
+    /* Service */
+    app.service('formService', [$http, function($http){
+        this.uploadFormDataToUrl = function(fd, url){
+            $http.post(url, fd, {
+                transformRequest : angular.identity,
+                headers : {'Content-type' : undefined}
+            });
+        };
+    }]);
+
+    /* Controller */
+    app.controller('csvController',['$scope', 'fileService', function($scope, fileService){
+        $scope.submitCSVData = function(){
+            $scope.uploadSuccess = undefined;
+            var file = $scope.csvFile;
+            console.log('File is ');
+            console.dir(file);
+            var url = "/rest/csv/uploadFile";
+            var fd = new FormData();
+            fd.append('file', file);
+            fd.append('name', name);
+            formService.uploadFormDataToUrl(fd, url).success(function(){
+                $scope.uploadSuccess = true;
+            }).error(function(){
+                $scope.uploadSuccess = false;
+            });
+
+        }
+    }]);
+})();
